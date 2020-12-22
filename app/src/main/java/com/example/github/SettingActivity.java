0099c2ff352da52beccf7f772d5c507f2c707f22
Tara Kit <darakit069@gmail.com>
2020-12-22 15:26:54 +0700

    update new method

--- app/src/main/java/com/example/github/SettingActivity.java
+++ app/src/main/java/com/example/github/SettingActivity.java
@@ -3,6 +3,7 @@ package com.example.github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    public void method(){
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
