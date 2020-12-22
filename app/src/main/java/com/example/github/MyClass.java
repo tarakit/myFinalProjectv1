a77e4643dab29f874196a797f6cfcf465b8e11f9
Tara Kit <darakit069@gmail.com>
2020-12-22 15:18:31 +0700

    add new class to Project

--- app/src/main/java/com/example/github/MyClass.java
+++ app/src/main/java/com/example/github/MyClass.java
@@ -1,4 +1,9 @@
 package com.example.github;
 
+import android.util.Log;
+
 public class MyClass {
+    public void mymethod(){
+        Log.d("TAG", "mymethod: ");
+    }
 }
---
+++ app/src/main/java/com/example/github/NewClass.java
@@ -0,0 +1,5 @@
+package com.example.github;
+
+public class NewClass {
+    // my code here
+}
